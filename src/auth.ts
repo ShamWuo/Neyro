dc69fbeb338b215bea25d92d62f14632c62d2b75
Sam Wu <abcdefggummybears0518@gmail.com>
2026-01-10 19:07:31 -0700

    #2

--- src/auth.ts
+++ src/auth.ts
@@ -12,6 +12,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "database" },
   secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
+  trustHost: true,
+  basePath: "/api/auth",
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
